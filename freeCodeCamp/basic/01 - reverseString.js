57988abc61f6dbbf119d1cb8912953d2c03521e2
apellanr <ryan.apellanes@gmail.com>
2017-09-16 09:21:29 -0700

    native array methods for reverse string

--- freeCodeCamp/basic/01 - reverseString.js
+++ freeCodeCamp/basic/01 - reverseString.js
@@ -2,17 +2,33 @@
 Reverse the provided string.
 You may need to turn into an array before you can reverse it.
 Result must be a string.
-
 */
 
+// Chaining
 function reverseString(str) {
-    // First, we need to use .split to change the string into an array so it can be further manipulated.
-    // Passing "" (an empty string) into the method splits the string at every character.
-    // .reverse method reverses the order of the elements in our newly formed array.
-    // .join method to reassemble our array back into a string. 
-    var reverseStr = str.split("").reverse().join(""); 
-    return reverseStr;
+    return str.split("").reverse().join(""); 
 }
+
+// Native Array Methods
+function reverseString(str) {
+    // First, we need to use .split method to change the string into 
+    // an array so it can be further manipulated.
+    // Passing "" (an empty string) into the method splits the string at every character.
+    var str = str.split("");
+
+    // .reverse method reverses the order of the elements in our newly formed array.
+    str = str.reverse();
+
+    // .join method to reassemble our array back into a string. 
+    // passing in empty string ensures nothing is placed between each array element
+    str = str.join("");
+
+    // return modified str
+    return str;
+}
+
+// No Array Methods
+
 
 console.log(reverseString("hello")); // "olleh"
 console.log(reverseString("Howdy")); // "ydowH"
