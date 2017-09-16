b9542b6a33147e4897633f1a4ee947c8d4d7e332
apellanr <ryan.apellanes@gmail.com>
2017-09-16 02:41:53 -0700

    code comments

--- freeCodeCamp/basic/01 - reverseString.js
+++ freeCodeCamp/basic/01 - reverseString.js
@@ -6,10 +6,15 @@ Result must be a string.
 */
 
 function reverseString(str) {
+    // First, we need to use .split to change the string into an array so it can be further manipulated.
+    // Passing "" (an empty string) into the method splits the string at every character.
+    // .reverse method reverses the order of the elements in our newly formed array.
+    // .join method to reassemble our array back into a string. 
     var reverseStr = str.split("").reverse().join(); 
+    return reverseStr;
 }
 
 console.log(reverseString("hello")); // "olleh"
-console.log(reverseString("Howdy")); // "Howdy"
-console.log(reverseString("Greetings from Earth")) // "htraE morf); sgniteerG"
-console.log(reverseString("Halt. Who goes there")); // "ereht seog); ohW .tlaH"
+console.log(reverseString("Howdy")); // "ydowH"
+console.log(reverseString("Greetings from Earth")); // "htraE morf sgniteerG"
+console.log(reverseString("Halt. Who goes there")); // "ereht seog ohW .tlaH"
