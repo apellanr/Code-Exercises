5b135a3892a58ae9a4bcb84cd415e7a82d2d255e
apellanr <ryan.apellanes@gmail.com>
2017-09-16 09:42:00 -0700

    last option with a decrementing loop

--- freeCodeCamp/basic/01 - reverseString.js
+++ freeCodeCamp/basic/01 - reverseString.js
@@ -11,6 +11,7 @@ function reverseString(str) {
 
 // Native Array Methods
 function reverseString(str) {
+    
     // First, we need to use .split method to change the string into 
     // an array so it can be further manipulated.
     // Passing "" (an empty string) into the method splits the string at every character.
@@ -28,8 +29,20 @@ function reverseString(str) {
 }
 
 // No Array Methods
+function reverseStr(str) {
+    var newString = "";
+    
+    // loop starts at the end of the string and stops at the first letter
+    // will store each consecutive letter in reverseStr variable
+    for(var i = str.length - 1; i >= 0; i--) {
+        console.log(i);
+        newString += str[i];
+        return newString;
+    }
+}
 
 
+// Test Cases
 console.log(reverseString("hello")); // "olleh"
 console.log(reverseString("Howdy")); // "ydowH"
 console.log(reverseString("Greetings from Earth")); // "htraE morf sgniteerG"
